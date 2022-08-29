cf2ce61eda88c6b06962b0743ff9cc77a2bd12dc
tatenodev <gd28.c.tateno@gmail.com>
2022-08-29 17:50:27 +0900

    wallet接続

--- dapp-starter-project/src/App.tsx
+++ dapp-starter-project/src/App.tsx
@@ -1,6 +1,52 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  // ユーザーのpublic wallet
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
+
+  /** window.ethereumにアクセス可能か確認 */
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) return console.log("Make sure you have MetaMask!");
+      console.log("We have the ethereum object", ethereum);
+
+      /** ユーザーのwalletへアクセスが許可されているかどうか */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  /** walletの接続 */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) return alert("Get MetaMask!");
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="mainContainer">
       <div className="dataContainer">
@@ -22,10 +68,13 @@ function App() {
           </span>
         </div>
 
-        {/* <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={() => null}>
           Wave at Me
-        </button> */}
-        <button className="waveButton">Wave at Me</button>
+        </button>
+
+        <button className="waveButton" onClick={connectWallet}>
+          {currentAccount ? "Wallet Connected" : "Connect Wallet"}
+        </button>
       </div>
     </div>
   );
