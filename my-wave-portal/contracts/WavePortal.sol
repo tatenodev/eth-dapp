b2d0590b8d9cc06acadd863d4911a71a5b0e5d74
tatenodev <gd28.c.tateno@gmail.com>
2022-08-30 12:16:29 +0900

    ethの受け取りをランダムにする

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -6,6 +6,9 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
 
+  // 乱数生成のための基盤となるシード
+  uint256 private seed;
+
   event NewWave(address indexed from, uint256 timestamp, string message);
 
   struct Wave {
@@ -18,23 +21,35 @@ contract WavePortal {
   
   constructor() payable {
     console.log("WavePortal - Smart Contract!");
+
+    // 初期シード設定
+    seed = (block.timestamp + block.difficulty) % 100;
   }
 
   function wave(string memory _message) public {
     totalWaves += 1;
     console.log("%s waved w/ message %s", msg.sender, _message);
-    // waveとmessageを配列に格納
     waves.push(Wave(msg.sender, _message, block.timestamp));
-    emit NewWave(msg.sender, block.timestamp, _message);
 
-    // waveを送ったユーザーに0.0001ethを送る
-    uint256 prizeAmout = 0.0001 ether;
-    require(prizeAmout <= address(this).balance, "Trying to withdraw more money than the contract has.");
-    (bool success, ) = (msg.sender).call{value: prizeAmout}("");
-    require(success, "Failed to withdraw money from contract.");
+    // ユーザーのために乱数を生成
+    seed = (block.difficulty + block.timestamp + seed) % 100;
+    console.log("Random # generated: %d", seed);
+
+    // ユーザーがethを獲得する確率を50%に設定
+    if (seed < 50) {
+      console.log("%s won!", msg.sender);
+      // waveを送ったユーザーに0.0001ethを送る
+      uint256 prizeAmout = 0.0001 ether;
+      require(prizeAmout <= address(this).balance, "Trying to withdraw more money than the contract has.");
+      (bool success, ) = (msg.sender).call{value: prizeAmout}("");
+      require(success, "Failed to withdraw money from contract.");
+    } else {
+      console.log("%s did not win.", msg.sender);
+    }
+    
+    emit NewWave(msg.sender, block.timestamp, _message);
   }
 
-  // wavesを返す
   function getAllWaves() public view returns (Wave[] memory) {
     return waves;
   }
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -12,9 +12,11 @@ const main = async () => {
   let contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
   console.log("Contract balance: ", hre.ethers.utils.formatEther(contractBalance));
 
-  // waveし、トランザクションが完了するまで待機
-  let waveTxn = await waveContract.wave("A message!");
-  await waveTxn.wait();
+  // 2回 waves を送る
+  let waveTxn1 = await waveContract.wave("This is wave #1");
+  await waveTxn1.wait();
+  let waveTxn2 = await waveContract.wave("This is wave #2");
+  await waveTxn2.wait();
 
   // waveした後のコントラクトの残高を取得し、結果を出力(0.0001引かれているか確認)
   contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
